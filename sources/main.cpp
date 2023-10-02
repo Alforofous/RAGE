bf3c578a6d22601094d40b1469848ef6728740ef
Alforofus <davidm.22@hotmail.com>
2023-10-02 15:03:45 +0300

    Able to open glfw window.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,23 @@
+#include "GLFW/glfw3.h"
+#include <stdio.h>
+
+static void error_callback(int error, const char *description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
 
 int main(void)
 {
+	if (glfwInit() == GLFW_FALSE)
+		return (1);
+	GLFWmonitor	*primary_monitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode *mode = glfwGetVideoMode(primary_monitor);
+	GLFWwindow	*glfw_window = glfwCreateWindow(mode->width, mode->height, "RAGE", NULL, NULL);
+	glfwSetErrorCallback(error_callback);
+	while (!glfwWindowShouldClose(glfw_window))
+	{
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return (0);
 }
