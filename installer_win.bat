66291d2bf03f5026436c77466a29b71a14d0b8c4
Alforofus <davidm.22@hotmail.com>
2023-10-06 19:51:05 +0300

    Drawing a first triangle from tutorial

--- installer_win.bat
+++ installer_win.bat
@@ -3,12 +3,15 @@ if "%1%" == "clean" (
 	echo Cleaning builds
 	rmdir /s /q "build" && echo removed build
 	rmdir /s /q "libraries/glfw-3.3.8/" && echo removed glfw
+	rmdir /s /q "libraries/glad/" && echo removed glad
 ) else if "%1%" == "test" (
 	echo Testing
 ) else (
 	REM Unpack and CMake glfw
 	echo Unpacking glfw...
 	tar -xf ./libraries/compressed/glfw-3.3.8.zip -C ./libraries/
+	tar -xf ./libraries/compressed/glad.zip -C ./libraries/
+	tar -xf ./libraries/compressed/glm.zip -C ./libraries/
 	REM CMake RAGE
 	cmake -B ./build -S ./
 	cmake --build ./build
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,9 @@
-#include <stdio.h>
+/*
+#define GLFW_INCLUDE_NONE
 #include "RAGE.hpp"
+#include "glad/glad.h"
+#include "glm/glm.hpp"
+#include <iostream>
 
 static void error_callback(int error, const char *description)
 {
@@ -14,10 +18,154 @@ int main(void)
 	if (rage->window->Init() == -1)
 		return (1);
 	glfwSetErrorCallback(error_callback);
+	glViewport(0, 0, rage->window->mode->width, rage->window->mode->height);
 	while (!glfwWindowShouldClose(rage->window->window))
 	{
+		glClearColor(1.0f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
+		glfwSwapBuffers(rage->window->window);
 	}
 	glfwTerminate();
 	return (0);
+}
+*/
+
+#define GLFW_INCLUDE_NONE
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static const struct
+{
+	float x, y;
+	float r, g, b;
+} vertices[3] =
+{
+	{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
+	{  0.6f, -0.4f, 0.f, 1.f, 0.f },
+	{   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char *vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char *fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+static void error_callback(int error, const char *description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+#include "glm/ext.hpp"
+
+int main(void)
+{
+	GLFWwindow *window;
+	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+	GLint mvp_location, vpos_location, vcol_location;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	gladLoadGL();
+
+	// NOTE: OpenGL error checks have been omitted for brevity
+
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	glCompileShader(vertex_shader);
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	glCompileShader(fragment_shader);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	mvp_location = glGetUniformLocation(program, "MVP");
+	vpos_location = glGetAttribLocation(program, "vPos");
+	vcol_location = glGetAttribLocation(program, "vCol");
+
+	glEnableVertexAttribArray(vpos_location);
+	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void *)0);
+	glEnableVertexAttribArray(vcol_location);
+	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void *)(sizeof(float) * 2));
+
+	while (!glfwWindowShouldClose(window))
+	{
+		float ratio;
+		int width, height;
+		glm::mat4 matrix(1.0f);
+
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		matrix = glm::rotate(matrix, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+		glm::mat4 projection_matrix = glm::ortho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+		glm::mat4 product_matrix = matrix * projection_matrix;
+
+		glUseProgram(program);
+		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(product_matrix));
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
