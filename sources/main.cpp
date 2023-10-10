97dd0bc8060ede692e9fcab64173f1ce208d9920
Alforofus <davidm.22@hotmail.com>
2023-10-10 11:37:21 +0300

    Managed to add NANOGUI example properly to draw GUI window on top of my own rendered shader scene.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,22 +1,30 @@
 #include "RAGE.hpp"
-#include "main.h"
+#include "nanogui/nanogui.h"
+
+using namespace nanogui;
+
+Screen *nanogui_screen = NULL;
 
 static void error_callback(int error, const char *description)
 {
 	fprintf(stderr, "Error: %s\n", description);
 }
 
-static void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+static void mouse_button_callback(GLFWwindow *window, int button, int action, int modifiers)
 {
 	RAGE	*rage = (RAGE *)glfwGetWindowUserPointer(window);
 	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
 		rage->camera->rotating_mode = true;
+
+	nanogui_screen->mouseButtonCallbackEvent(button, action, modifiers);
 }
 
-static void cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
+static void cursor_position_callback(GLFWwindow *window, double x, double y)
 {
 	RAGE	*rage = (RAGE *)glfwGetWindowUserPointer(window);
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+//	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	nanogui_screen->cursorPosCallbackEvent(x, y);
 }
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
@@ -26,9 +34,9 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 		rage->camera->MoveLocaly(glm::vec3(0.0f, 0.0f, 1.0f));
 	if (key == GLFW_KEY_S && action == GLFW_PRESS)
 		rage->camera->MoveLocaly(glm::vec3(0.0f, 0.0f, -1.0f));
-}
 
-#include <nanogui/nanogui.h>
+	nanogui_screen->keyCallbackEvent(key, scancode, action, mods);
+}
 
 int main(void)
 {
@@ -52,8 +60,45 @@ int main(void)
 	ShaderLoader shaderLoader("../shaders/vertex_test.glsl", "../shaders/fragment_test.glsl");
 
 	/*Load viewport and start using shader program*/
-	glViewport(0, 0, rage->window->mode->width, rage->window->mode->height);
-	glUseProgram(shaderLoader.hProgram);
+
+	nanogui_screen = new Screen();
+	nanogui_screen->initialize(rage->window->glfw_window, true);
+
+	bool bvar = true;
+
+	bool enabled = true;
+	FormHelper *gui = new FormHelper(nanogui_screen);
+	ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+	gui->addGroup("Basic types");
+	gui->addVariable("bool", bvar)->setTooltip("Test tooltip.");
+
+	nanogui_screen->setVisible(true);
+	nanogui_screen->performLayout();
+	nanoguiWindow->center();
+
+	glfwSetCharCallback(rage->window->glfw_window,
+		[](GLFWwindow *, unsigned int codepoint) {
+			nanogui_screen->charCallbackEvent(codepoint);
+		}
+	);
+
+	glfwSetDropCallback(rage->window->glfw_window,
+		[](GLFWwindow *, int count, const char **filenames) {
+			nanogui_screen->dropCallbackEvent(count, filenames);
+		}
+	);
+
+	glfwSetScrollCallback(rage->window->glfw_window,
+		[](GLFWwindow *, double x, double y) {
+			nanogui_screen->scrollCallbackEvent(x, y);
+		}
+	);
+
+	glfwSetFramebufferSizeCallback(rage->window->glfw_window,
+		[](GLFWwindow *, int width, int height) {
+			nanogui_screen->resizeCallbackEvent(width, height);
+		}
+	);
 
 	/*Set GLSL variable locations*/
 	int resolutionLocation = glGetUniformLocation(shaderLoader.hProgram, "u_resolution");
@@ -65,7 +110,10 @@ int main(void)
 	rage->camera->SetPosition(glm::vec3(0.0, 0.0, -3.0));
 	while (!glfwWindowShouldClose(rage->window->glfw_window))
 	{
+		glfwPollEvents();
 		glfwGetWindowSize(rage->window->glfw_window, &width, &height);
+		glViewport(0, 0, width, height);
+		glUseProgram(shaderLoader.hProgram);
 		glm::vec3 camera_rotation = rage->camera->GetRotation();
 		if (rage->camera->rotating_mode == true)
 			 rage->camera->SetRotation(camera_rotation + glm::vec3(0.1f, 0.0f, 0.0f));
@@ -78,9 +126,11 @@ int main(void)
 		glUniform2f(resolutionLocation, (float)width, (float)height);
 		glUniform3f(cameraPositionLocation, camera_position.x, camera_position.y, camera_position.z);
 		glUniform3f(cameraDirectionLocation, camera_direction.x, camera_direction.y, camera_direction.z);
+		glClearColor(0.03f, 0.04f, 0.07f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glfwPollEvents();
+		nanogui_screen->drawContents();
+		nanogui_screen->drawWidgets();
 		glfwSwapBuffers(rage->window->glfw_window);
 	}
 	delete rage;
