169f5d03598552baecf1564cace658f390ab85f6
Alforofus <davidm.22@hotmail.com>
2023-10-28 19:00:11 +0300

    Added index buffer (EBO - element buffer object).

--- sources/main.cpp
+++ sources/main.cpp
@@ -18,11 +18,23 @@ int main(void)
 	glfwMakeContextCurrent(rage->window->glfw_window);
 	gladLoadGL();
 
+	// Vertices coordinates
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Upper triangle
+		5, 4, 1 // Lower right triangle
 	};
 
 	set_callbacks(rage);
@@ -33,29 +45,32 @@ int main(void)
 
 	rage->shader->InitVariableLocations();
 
-	GLuint vertex_array_object;
-	GLuint vertex_buffer_object;
+	GLuint	vertex_array_object;
+	GLuint	vertex_buffer_object;
+	GLuint	element_buffer_object;
 
 	glGenVertexArrays(1, &vertex_array_object);
 	glGenBuffers(1, &vertex_buffer_object);
+	glGenBuffers(1, &element_buffer_object);
 
 	glBindVertexArray(vertex_array_object);
-
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	rage->camera->SetPosition(glm::vec3(0.0, 0.0, -3.0));
 	while (!glfwWindowShouldClose(rage->window->glfw_window))
 	{
 		clock_t beginFrame = clock();
 
-		glfwPollEvents();
 		int width;
 		int height;
 		glfwGetWindowSize(rage->window->glfw_window, &width, &height);
@@ -65,7 +80,8 @@ int main(void)
 		glUseProgram(rage->shader->hProgram);
 		glBindVertexArray(vertex_array_object);
 		set_shader_variable_values(rage);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glfwPollEvents();
 		rage->gui->draw(rage);
 		glfwSwapBuffers(rage->window->glfw_window);
 
