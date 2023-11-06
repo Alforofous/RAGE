8d34b354568a282591ad14ea4d9754635828c4e3
David Malesevic <davidm.22@hotmail.com>
2023-11-06 15:52:14 +0200

    Managed to find out why application exits on macOS.

--- sources/RAGE_window.cpp
+++ sources/RAGE_window.cpp
@@ -4,13 +4,22 @@ int RAGE_window::Init()
 {
 	primary_monitor = glfwGetPrimaryMonitor();
 	if (primary_monitor == NULL)
+	{
+		std::cerr << "Failed to get primary monitor" << std::endl;
 		return (-1);
+	}
 	mode = glfwGetVideoMode(primary_monitor);
 	if (mode == NULL)
+	{
+		std::cerr << "Failed to get video mode" << std::endl;
 		return (-1);
+	}
 	glfw_window = glfwCreateWindow(mode->width / 2, mode->height / 2, "RAGE", NULL, NULL);
 	if (glfw_window == NULL)
+	{
+		std::cerr << "Failed to create window" << std::endl;
 		return (-1);
+	}
 	return (1);
 }
 
--- sources/main.cpp
+++ sources/main.cpp
@@ -10,6 +10,7 @@ int main(void)
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	if (rage->window->Init() == -1)
