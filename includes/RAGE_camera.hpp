113abca27d3d2ad36639c774078e2063e9a4ced5
David Malesevic <davidm.22@hotmail.com>
2023-12-13 13:36:42 +0200

    Refactor RAGE_camera constructor and add projection matrix calculation

--- includes/RAGE_camera.hpp
+++ includes/RAGE_camera.hpp
@@ -5,13 +5,17 @@
 class RAGE_camera : public RAGE_object
 {
 public:
-	glm::mat4	m_view;
-	RAGE_camera();
+	RAGE_camera(float fov = 45.0f, glm::vec2 window_size = glm::vec2(800, 600), glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2 z_plane = glm::vec2(0.1f, 100.0f));
+	glm::mat4 m_view;
 	void move_localy(glm::vec3 move);
 	void update_view();
 	bool rotating_mode;
+
 private:
-	glm::vec3	m_forward;
-	glm::vec3	m_up;
-	glm::vec3	m_right;
+	glm::vec3 m_forward;
+	glm::vec3 m_up;
+	glm::vec3 m_right;
+	float m_aspect_ratio;
+	float m_fov;
+	glm::mat4 m_projection_matrix;
 };
--- sources/RAGE_camera.cpp
+++ sources/RAGE_camera.cpp
@@ -1,12 +1,17 @@
 #include "RAGE.hpp"
+#include "RAGE_camera.hpp"
 
-RAGE_camera::RAGE_camera()
+RAGE_camera::RAGE_camera(float fov, glm::vec2 window_size, glm::vec3 position, glm::vec3 rotation, glm::vec2 z_plane)
 {
-	glm::vec3 initial_rotation = glm::vec3(0.0f, -90.0f, 0.0f);
-	SetRotation(initial_rotation);
-	m_position = glm::vec3(0.0f, 0.0f, 3.0f);
+	m_position = position;
 	m_forward = glm::vec3(0.0f, 0.0f, -1.0f);
 	m_up = glm::vec3(0.0f, 1.0f, 0.0f);
+	m_right = glm::vec3(1.0f, 0.0f, 0.0f);
+	m_fov = fov;
+	m_aspect_ratio = window_size.x / window_size.y;
+	m_rotation = rotation;
+	m_view = glm::lookAt(m_position, m_position + m_forward, m_up);
+	m_projection_matrix = glm::perspective(m_fov, m_aspect_ratio, z_plane.x, z_plane.y);
 }
 
 void RAGE_camera::update_view()
