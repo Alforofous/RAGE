f2181e788655905ad76cf5aa79cbd355066b22b7
David Malesevic <davidm.22@hotmail.com>
2023-12-23 20:27:40 +0200

    Add position input and slider for selected objects

--- sources/gui/RAGE_inspector.cpp
+++ sources/gui/RAGE_inspector.cpp
@@ -8,6 +8,18 @@ void RAGE_inspector::draw(RAGE *rage)
 	std::vector<RAGE_object *> *selected_objects = rage->scene.get_selected_objects();
 	ImGui::Text("Selected object count: %zu", selected_objects->size());
 	for (int i = 0; i < selected_objects->size(); i++)
+	{
 		ImGui::Text("Selected object name: %s", selected_objects->at(i)->get_name().c_str());
+
+		glm::vec3 position = selected_objects->at(i)->get_position();
+		if (ImGui::InputFloat3("Position Input", glm::value_ptr(position)))
+		{
+			selected_objects->at(i)->set_position(position);
+		}
+		if (ImGui::SliderFloat3("Position Slider", glm::value_ptr(position), -100.0f, 100.0f))
+		{
+			selected_objects->at(i)->set_position(position);
+		}
+	}
 	ImGui::End();
 }
