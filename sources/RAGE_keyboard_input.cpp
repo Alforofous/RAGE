7164dd559c488cb4345c51d03c4930b3b2a5f40a
David Malesevic <davidm.22@hotmail.com>
2023-12-25 11:30:56 +0200

    Remove F1 key functionality from RAGE_keyboard_input.cpp

--- sources/RAGE_keyboard_input.cpp
+++ sources/RAGE_keyboard_input.cpp
@@ -4,8 +4,6 @@ void RAGE_keyboard_input::signal(void *param, int key)
 {
 	RAGE *rage = (RAGE *)param;
 
-	if (key == GLFW_KEY_F1)
-		rage->scene.print_objects();
 	if (key == GLFW_KEY_F3)
 		rage->gui->show_performance_window = !rage->gui->show_performance_window;
 	if (key == GLFW_KEY_ESCAPE)
